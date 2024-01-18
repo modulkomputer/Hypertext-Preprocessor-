7032fe1db85743cbd010dfe750f777f68c2d2bbd
mhaemnn <muhaemin1998@gmail.com>
2024-01-19 01:05:26 +0700

    menambahkan folder get & post

--- #6 Get & Post/latihan1.php
+++ #6 Get & Post/latihan1.php
@@ -1,23 +1,20 @@
-<?php
-// belajar metod GET
-$nama = $_GET["name"];
-$umur = $_GET["umur"];
-?>
 <html>
 
 <head>
-  <title>Metode GET</title>
+  <title>Metode POST</title>
 </head>
 
 <body>
-  <form action="" method="GET">
-    <input type="text" name="name" placeholder="Masukan Nama"> <br>
+  <form action="" method="post">
+    <input type="text" name="nama" placeholder="Masukan Nama"> <br>
     <input type="text" name="umur" placeholder="Masukan Umur"> <br>
-    <button type="submit">Kirim</button> <br>
-
-    <p>Nama Saya adalah <?php echo $nama; ?> </p>
-    <p>Dan Umur saya adalah <?php echo $umur; ?></p>
+    <button type="submit" name="submit">Kirim</button> <br>
   </form>
+  <!-- jika tombol submit sudah di tekan tapilakan tag <p>, kalau belum kosong -->
+  <?php if (isset($_POST["submit"])) : ?>
+    <p>Hallo, selamat datang <?php echo $_POST["nama"]; ?> </p>
+    <p>Umur saya adalah <?php echo $_POST["umur"]; ?></p>
+  <?php endif; ?>
 </body>
 
 </html>
